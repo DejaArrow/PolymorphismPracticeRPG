01a2e46df102ffe8ff51b662ecc6415e9871f944
Debbie Green <P110122341@student.peterborough.ac.uk>
2020-10-22 12:52:54 +0100

    AddedBasicClasses

---
+++ RPGPracticeCode/Character.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace RPGPracticeCode
+{
+    class Character
+    {
+        public int TakeDamage(int)
+        {
+
+        }
+
+        public int DoDamage(int)
+        {
+            
+        }
+
+        public int ReceiveHealing(int)
+        {
+
+        }
+
+        public int AbilityModifier(int)
+        {
+
+        }
+
+        public string ChangeSize()
+        {
+
+        }
+
+        public string
+
+
+
+
+
+    }
+}
---
+++ RPGPracticeCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPGPracticeCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
