7b5ea5dc7c0e84f2c77bac3c09f8f72a1a64fe30
Debbie Green <P110122341@student.peterborough.ac.uk>
2020-10-29 08:46:13 +0000

    Construct started

--- RPGPracticeCode/Character.cs
+++ RPGPracticeCode/Character.cs
@@ -3,27 +3,31 @@ using System;
 namespace RPGPracticeCode
 {
     class Character
+    {
     
     public string name; //name of player or mob
     public string race; //such as human, elf, ogre, troll etc
     public int level;   //level of human or mob
-    public int hitpoints; //how much hp character has
-    public int strength; //strength = base attack bonus, also required for some skill checks
-    public int intelligence; //provides intelligence modifier for skill checks
-    public int charisma; //provides charisma modifier for skill checks
-    public int constitution; //provides consitution modifier for hitpoints and saves
-    public int wisdom; //provides wisom modifier for skill checks and saves 
+    public int hitPoints; //how much hp character has  
     public int armourClass; //provides ac when attacked 
     public string size; //size of character if environment needs to know
     public string language; //language spoken by Character
     public int movementSpeed; //how fast a character moves. 
+    public int strength; //strength = base attack bonus, also required for some skill checks
+    public int intelligence; //provides intelligence modifier for skill checks
+    public int charisma; //provides charisma modifier for skill checks
+    public int constitution; //provides constitution modifier for hitpoints and saves
+    public int wisdom; //provides wisom modifier for skill checks and saves 
+ 
+  
 
 
 
-    {
+    
         public string TargetToAttack()
         {
             //Targets character
+            
         }
         public string TargetToHeal()
         {
@@ -64,9 +68,26 @@ namespace RPGPracticeCode
             //Alive or Dead
         }
 
+        public Character (string name, string race, int level, int hitPoints, int armourClass, string size, string language, int movementSpeed, int strength, int intelligence, int charisma, int constitution, int wisdom)
+        {
+                this.name = name;
+                this.race = race;
+                this.level = level;
+                this.hitPoints = hitPoints;
+                this.armourClass = armourClass;
+                this.size = size;
+                this.language = language;
+                this.movementSpeed = movementSpeed;
+                this.strength = strength;
+                this.intelligence = intelligence;
+                this.charisma = charisma;
+                this.constitution = constitution;
+                this.wisdom = wisdom;
+
+        }
 
 
-
-
+    
     }
+
 }
